723c332ca0577ba40cf75b4c9567c9c4a269c0c2
Aleksandar Stojanovic <astojanovic17@raf.rs>
2019-12-14 16:34:35 +0100

    stylesheet import progress

--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-context = {
-			'logTitle': 'Uloguj se',
-			'regTitle': 'Registruj se',
-		}
 
 def auth(request):
-		return render(request, 'posts/auth.html', context)
+    return render(request, 'posts/auth.html', {'login': 'Uloguj se', 'register': 'Registruj se'})
+
 
 def board(request):
-		return render(request, 'posts/board.html')
+    return render(request, 'posts/board.html')
+
 
 def profile(request):
-    	return render(request, 'posts/profile.html')
+    return render(request, 'posts/profile.html')
+
 
 def new_post(request):
-		return render(request, 'posts/new_post.html')
-
+    return render(request, 'posts/new_post.html')
