cf364114cef054b4ea2f81bd21276e2639f0e502
Aleksandar Stojanovic <astojanovic17@raf.rs>
2019-12-17 22:27:24 +0100

    Logic refactor
    Added support for edit and delete posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from . import views
-from .views import (PostListView, PostCreateView)
+from .views import (PostListView, PostCreateView, PostProfileView, PostUpdateView, PostDeleteView)
 
 #Rutiranje za posts aplikaciju
 urlpatterns = [
     path('', PostListView.as_view(), name = 'posts-board'),
-    path('profile/', views.profile, name = 'posts-profile'),
+    #path('profile/', views.profile, name = 'posts-profile'),
+    path('profile/', PostProfileView.as_view(), name = 'posts-profile'),
     path('new_post/', PostCreateView.as_view(), name = 'posts-new_post'),
+    path('edit_post/<int:pk>/', PostUpdateView.as_view(), name = 'posts-edit_post'),
+    path('delete_post/<int:pk>/', PostDeleteView.as_view(), name = 'posts-delete_post'),
     path('logout/', views.logout, name = 'posts-logout'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,22 +1,68 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.views.generic import (ListView, CreateView)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView, CreateView, UpdateView, DeleteView)
 from django.urls import reverse_lazy
 from .models import Post
 
-class PostListView(ListView):
+
+# Prikaz glavne stranice sa svim postovima
+class PostListView(LoginRequiredMixin, ListView):
 	model = Post
 	template_name = 'posts/board.html'
 	context_object_name = 'posts'
 
-class PostCreateView(CreateView):
+# Prikaz stranice za dodavanje postova
+class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 	model = Post
 	fields = ['post_title', 'post_tweet']
 
 	def form_valid(self, form):
 		form.instance.post_user_id = self.request.user
 		return super().form_valid(form)
+
+	success_url = reverse_lazy('posts-board')
+
+# Prikaz stranice za update postova
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+	model = Post
+	fields = ['post_title', 'post_tweet']
+	template_name = 'posts/edit_post.html'
+
+	def form_valid(self, form):
+		form.instance.post_user_id = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.post_user_id:
+			return True
+		return False
+
+	success_url = reverse_lazy('posts-board')
+
+# Prikaz profila sa postovima
+class PostProfileView(ListView):
+	model = Post
+	template_name = 'posts/prfile.html'
+
+	def get_queryset(self):
+		queryset = super(PostProfileView, self).get_queryset()
+		queryset = queryset.filter(post_user_id = self.request.user)
+		return queryset
+
+	context_object_name = 'posts'
+
+# Brisanje posta
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Post
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.post_user_id:
+			return True
+		return False
 
 	success_url = reverse_lazy('posts-board')
 
