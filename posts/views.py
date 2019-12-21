ec44fdf440cc8b1016e7a69afdd6353d6d464ae0
Aleksandar Stojanovic <astojanovic17@raf.rs>
2019-12-21 14:09:12 +0100

    Final commit
    fixed search function

--- posts/views.py
+++ posts/views.py
@@ -6,11 +6,28 @@ from django.views.generic import (ListView, CreateView, UpdateView, DeleteView)
 from django.urls import reverse_lazy
 from .models import Post
 
+# Pomocna funkciaj za validaciju
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
 # Prikaz glavne stranice sa svim postovima
 class PostListView(LoginRequiredMixin, ListView):
 	model = Post
 	template_name = 'posts/board.html'
 	context_object_name = 'posts'
+
+	def get_queryset(self):
+		title = self.request.GET.get('title')
+		tweet = self.request.GET.get('tweet')
+		qs = super(PostListView, self).get_queryset()
+		
+		if is_valid_queryparam(title):
+			qs = qs.filter(post_title__icontains = title)
+
+		elif is_valid_queryparam(tweet):
+			qs = qs.filter(post_tweet__icontains = tweet)
+
+		return qs
 
 # Prikaz stranice za dodavanje postova
 class PostCreateView(LoginRequiredMixin, CreateView):
@@ -55,9 +72,18 @@ class PostProfileView(LoginRequiredMixin, ListView):
 
 	def get_queryset(self):
 		post_user_id = self.request.user
+		title = self.request.GET.get('title')
+		tweet = self.request.GET.get('tweet')
 		qs = super(PostProfileView, self).get_queryset()
-		return qs.filter(post_user_id = post_user_id)
 		
+		if is_valid_queryparam(title):
+			qs = qs.filter(post_title__icontains = title)
+
+		elif is_valid_queryparam(tweet):
+			qs = qs.filter(post_tweet__icontains = tweet)
+
+		return qs.filter(post_user_id = post_user_id)
+       
 # Brisanje posta
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 	model = Post
